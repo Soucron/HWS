01896ef9dda9af1986df3035ed5cc10ce8f79411
Iniok <ilidanilo@mail.ru>
2023-06-26 00:01:17 +0300

    HWS 12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,17 +1,26 @@
+export type InitStateType = {
+    themeId : number
+}
+
 const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state : InitStateType = initState, action: changeThemeIdAT) : InitStateType  => { // fix any
     switch (action.type) {
         // дописать
         case 'SET_THEME_ID' :
-            return state
+            return {...state, themeId: action.id}
 
 
         default:
             return state
     }
+}
+
+type changeThemeIdAT = {
+    type: 'SET_THEME_ID',
+    id: number
 }
 
 export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
